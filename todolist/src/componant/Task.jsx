e8caac3dbbc864dbdd0c5856f2a87514b20236ad
Nipun Madhushan <madhushanhmn.21@uom.lk>
2023-02-23 13:54:16 +0530

    delete
    del

--- todolist/src/componant/Task.jsx
+++ todolist/src/componant/Task.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import axios from 'axios';
+
+
 
 function Task({id,username,todoname}) {
+  function handledelete(){
+  axios.delete(`http://127.0.0.1:5000/api/todo/${id}`)
+  window.location.reload()
+
+
+  }
   return (
     <div >
           <p>{username} :
@@ -10,7 +19,7 @@ function Task({id,username,todoname}) {
                 {todoname}  
 
                 </p>
-                <div><DeleteIcon/></div>
+                <div><DeleteIcon  onClick={handledelete}/></div>
                 <CheckCircleOutlineIcon/>
 
                     
