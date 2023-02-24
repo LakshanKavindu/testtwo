0f571c9eb99810a2f9b698b18fc522d1ed9282f3
GPYW <pasindiyathra@gmail.com>
2023-02-24 13:17:42 +0530

    updated

--- todolist/src/pages/Adminsignin.jsx
+++ todolist/src/pages/Adminsignin.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import './pasindi.css'
 
 const Adminsignin = () => {
   return (
-    <div>Adminsignin</div>
-  )
+    <div class = "Sign in">
+      <div class = "split right">
+      <div class = "centered">
+      <h1 ><center>Sign In</center></h1>
+
+      <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required/>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required/>
+
+    <div class="clearfix">
+    <button type="submit" class="signupbtn">Sign In</button>
+     </div>
+     </div>
+     </div>
+     <div class="split left">
+   <div class="centered">
+    <h2>Jane Flex</h2>
+    <p>Some text.</p>
+   </div>
+   </div>
+    </div>
+    
+  );
 }
 
 export default Adminsignin
